fe712ebaeb59a60b147f107474b675f09bcafc36
prasadbatturi <prasadbatturi@gmail.com>
2022-11-21 15:06:54 +0530

    first commit

---
+++ Method/src/Launch1.java
@@ -0,0 +1,23 @@
+class Calc{
+	int a,b,c;
+	
+	void add()
+	{
+		a=10;;
+		b=20;
+		c=a+b;
+		System.out.println(c);
+	}
+}
+
+
+public class Launch1 {
+
+	public static void main(String[] args) {
+		
+   Calc c1=new Calc();
+   c1.add();
+
+	}
+
+}
